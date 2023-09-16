9880cd59a851c4cef278f4a9f4ddac659af3ffa4
Mauricio <mauricom7@hotmail.com>
2023-09-16 11:34:11 -0300

    Teste

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -13,8 +13,27 @@ export class ECommerceApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
         super(scope, id, props)
 
+
+
+        const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
+
         const api = new apigateway.RestApi(this, "ECommerceApi", {
-            restApiName: "ECommerceApi"
+            restApiName: "ECommerceApi",
+            deployOptions: {
+                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+                    caller: true,
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    user: true
+                    
+                })
+            }
         })
 
         const prodcutsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
